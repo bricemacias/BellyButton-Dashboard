32ce49e9c441614c50056e5c0ffa64a042f8e414
Brice Mvf <bricemacias@hotmail.com>
2021-02-01 16:21:43 +0100

    Initializing Apollo and StyledComponents

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const API_URL = process.env.REACT_APP_API_URL;
+const API_GUEST_KEY = process.env.REACT_APP_API_KEY;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  uri: API_URL,
+  cache: cache,
+  headers: {
+    authorization: `Bearer ${API_GUEST_KEY}`,
+  },
+});
+
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
